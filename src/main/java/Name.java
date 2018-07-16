a0f0ad16c57699eb7c0a7cb568811cd918a0a66e
Magdalena <m.mandat@gmail.com>
2018-07-16 17:18:52 +0100

    everything refactored

---
+++ src/main/java/Name.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static spark.Spark.get;
+
+public class Name {
+
+    private ArrayList<String> names;
+
+    public Name() {
+        names = new ArrayList<>();
+        names.add("Magda");
+        names.add("Helen");
+        names.add("Ben");
+        names.add("Gary");
+        names.add("Lee");
+
+    }
+
+    public void shuffleNames(){
+        Collections.shuffle(names);
+    }
+
+    public String randomName(){
+        shuffleNames();
+        return names.get(0);
+    }
+
+    public ArrayList<String> twoRandomNames(){
+        ArrayList<String> twoNames = new ArrayList<>();
+        shuffleNames();
+        twoNames.add(names.get(0));
+        twoNames.add(names.get(1));
+        return twoNames;
+    }
+
+    public ArrayList<String> threeRandomNames(){
+        ArrayList<String> threeNames = new ArrayList<>();
+        shuffleNames();
+        threeNames.add(names.get(0));
+        threeNames.add(names.get(1));
+        threeNames.add(names.get(2));
+
+        return threeNames;
+    }
+
+
+    public ArrayList<String> fourRandomNames(){
+        ArrayList<String> fourNames = new ArrayList<>();
+        shuffleNames();
+        fourNames.add(names.get(0));
+        fourNames.add(names.get(1));
+        fourNames.add(names.get(2));
+        fourNames.add(names.get(3));
+
+        return fourNames;
+    }
+
+    public ArrayList<String> getNames(){
+        return names;
+    }
+
+}
+
+
+
+
---
+++ src/main/java/NameController.java
@@ -0,0 +1,61 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class NameController {
+
+        public static void main(String[] args) {
+            VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+            staticFileLocation("/public");
+
+
+            get("/one", (req, res) -> {
+
+                Name name = new Name();
+                String result = name.randomName();
+
+                HashMap<String, Object> model = new HashMap<>();
+                model.put("name", result);
+                return new ModelAndView(model, "name.vtl");
+            }, velocityTemplateEngine);
+
+            get("/two", (req, res) -> {
+
+                Name name = new Name();
+                ArrayList<String> result = name.twoRandomNames();
+
+                HashMap<String, Object> model = new HashMap<>();
+                model.put("names", result);
+                return new ModelAndView(model, "twonames.vtl");
+            }, velocityTemplateEngine);
+
+            get("/three", (req, res) -> {
+
+                Name name = new Name();
+                ArrayList<String> result = name.threeRandomNames();
+
+                HashMap<String, Object> model = new HashMap<>();
+                model.put("names", result);
+                return new ModelAndView(model, "threenames.vtl");
+            }, velocityTemplateEngine);
+
+            get("/four", (req, res) -> {
+
+                Name name = new Name();
+                ArrayList<String> result = name.fourRandomNames();
+
+                HashMap<String, Object> model = new HashMap<>();
+                model.put("names", result);
+                return new ModelAndView(model, "fournames.vtl");
+            }, velocityTemplateEngine);
+
+        }
+}
+
+
+
